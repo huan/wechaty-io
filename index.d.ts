74663729fec7ecc29bee63c25c52210d09426796
Zhuohuan LI <zixia@zixia.net>
2016-07-26 01:15:27 +0800

    fix type difinition

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,10 @@
-// https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
-// https://github.com/Microsoft/TypeScript/issues/2076#issuecomment-75052599
+declare module 'wechaty-io' {
+  import * as http from 'http'
 
-import * as http from 'http'
+  interface IoServer {
+    new (server: http.Server): IoServer
+    init(): Promise<IoServer | void>
+  }
 
-interface IoServer {
-  new (server: http.Server): IoServer
-  init(): Promise<IoServer | void>
+  export const IoServer: IoServer
 }
-
-export const IoServer: IoServer
